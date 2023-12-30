a8359275c46d079b3c4754300f1639435ff84628
narfagames <104780672+narfagames@users.noreply.github.com>
2023-12-30 00:00:27 -0500

    Update consultarUsuario.php
    ff

--- consultarUsuario.php
+++ consultarUsuario.php
@@ -1,5 +1,10 @@
 <?php
-include('conexion.php');
+$server = "bljvi8izdfsqurtiwlnz-mysql.services.clever-cloud.com";
+$username = "ufc6hrwrcpv1iohv";
+$password = "gcd0i2mvkwlVUh2EJD4a";
+$db = "bljvi8izdfsqurtiwlnz";
+
+$connect = new mysqli($server,$username,$password,$db);
         $USUARIO = 'nruiz';
         $result = mysqli_query($connect,"SELECT * FROM T_M_USER WHERE usuario='$USUARIO'");
         $qCol= mysqli_field_count($connect);
